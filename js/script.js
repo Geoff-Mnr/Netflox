c7b480f39a7dc26ecc4d529d20bfd9092a3a1224
Yakuz0r <geoffrey_meunier@live.be>
2023-02-15 18:06:23 +0100

    variables renaming

--- js/script.js
+++ js/script.js
@@ -1,12 +1,12 @@
-document.querySelectorAll(".question__main").forEach((div) => {
+document.querySelectorAll(".question_main").forEach((div) => {
     div.addEventListener("click", (div) => {
       questionDiv = div.currentTarget;
-      answer = questionDiv.querySelector(".question__answer");
+      answer = questionDiv.querySelector(".question_answer");
       answer.style.display =
         answer.style.display == "none" || answer.style.display == ""
           ? "block"
           : "none";
-      switchButton = questionDiv.querySelector(".question__title__switch");
+      switchButton = questionDiv.querySelector(".question_title_switch");
       switchButton.classList.toggle("active");
     });
   });
