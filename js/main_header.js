715e452b68bfa790247ae49e23467e34ac8963e0
Yakuz0r <geoffrey_meunier@live.be>
2023-02-21 20:54:15 +0100

    add comment

--- js/main_header.js
+++ js/main_header.js
@@ -1,5 +1,8 @@
+//selection de toute la navbar
 const nav = document.querySelector('nav');
 
+//quand l'événement scroll est activé des que le scroll est inférieur ou egale à 50px
+//du coup ça active les propriété de la classe active_nav
 window.addEventListener('scroll', () => {
     if (window.scrollY >= 50) {
         nav.classList.add('active_nav');
--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+//selectionne tous les éléments de .question_main et ça boucle jusqu'a ce qu'on reclique dessus
 document.querySelectorAll(".question_main").forEach((div) => {
     div.addEventListener("click", (div) => {
       questionDiv = div.currentTarget;
